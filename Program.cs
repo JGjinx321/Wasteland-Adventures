9444bb8826fabb44d505b0d4d1cd2b2f0cace49d
JGjinx321 <50683258+JGjinx321@users.noreply.github.com>
2021-01-14 19:44:11 -0600

    The main program
    The beginning chapter. Next will be providing a selection for users to choose the jobs.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+namespace Littler_story
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The story begins with. . . Oh yes, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Awesome! " + name + " it is then");
+            Console.WriteLine("This story takes place in 2077. A few years after nuclear annihilation \n " +
+               "comes to the human race. Now we have to get you prepared for your survival \n" +
+                "in this new wasteland until your doom is inevitably upon you. \n" +
+                "By the way what was your profession before the wasteland?");
+
+
+
+        }
+    }
+}
