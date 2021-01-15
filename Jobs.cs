933ebb079cabdc677be866eba5b5cd57581d7d7b
JGjinx321 <50683258+JGjinx321@users.noreply.github.com>
2021-01-14 19:44:46 -0600

    Add files via upload
    Profession selection

---
+++ Jobs.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Littler_story
+{
+    class FireFighter
+    {
+        public int Strength = 5;
+        public int Intelligence = 2;
+        public int Agility = 3;
+        public int Charisma = 3;
+        public int health = 100;
+    }
+    class PoliceOfficer
+    {
+        public int Strength = 4;
+        public int Intelligence = 3;
+        public int Agility = 4;
+        public int Charisma = 2;
+        public int health = 100;
+    }
+    class Surivalist
+    {
+        public int Strength = 3;
+        public int Intelligence = 5;
+        public int Agility = 4;
+        public int Charisma = 1;
+        public int health = 100;
+    }
+    class Actor
+    {
+        public int Strength = 2;
+        public int Intelligence = 4;
+        public int Agility = 2;
+        public int Charisma = 5;
+        public int health = 100;
+    }
+    class Unemployed
+    {
+        public int Strength = 3;
+        public int Intelligence = 3;
+        public int Agility = 3;
+        public int Charisma = 4;
+        public int health = 100;
+    }
+}
