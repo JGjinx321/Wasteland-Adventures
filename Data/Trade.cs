4aa5ad44f669ad9c016a52a3e893e3d19af358ac
Jaret Sanchez <jsanchez@icstars.org>
2022-05-09 17:22:35 -0400

    Add project files.

---
+++ Data/Trade.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Wasteland_Adventures.Data
+{
+    public class Trade
+    {
+        public string name { get; set; }
+
+        public int Strength { get; set; }
+
+        public int Intelligence { get; set; }
+
+        public int Agility { get; set; }
+
+        public int Perception { get; set; }
+
+        public int Weaponry { get; set; }
+
+        public int Charisma { get; set; }
+
+
+        public static Trade PlayerSelection(int selection)
+        {
+            List<Trade> trades = new List<Trade>();
+
+            if(selection == 0)
+            {
+                Trade Firefighter = new Trade();
+
+                Firefighter.name = "Fire Fighter";
+
+                Firefighter.Agility = 4;
+
+                Firefighter.Charisma = 3;
+
+                Firefighter.Intelligence = 3;
+
+                Firefighter.Perception = 2;
+
+                Firefighter.Strength = 4;
+
+                Firefighter.Weaponry = 2;
+
+
+                trades.Add(Firefighter);
+            }
+
+            if(selection == 1)
+            {
+                Trade PoliceOfficer = new Trade();
+
+                PoliceOfficer.name = "Police Officer";
+
+                PoliceOfficer.Agility = 3;
+
+                PoliceOfficer.Charisma = 2;
+
+                PoliceOfficer.Intelligence = 3;
+
+                PoliceOfficer.Perception = 3;
+
+                PoliceOfficer.Strength = 3;
+
+                PoliceOfficer.Weaponry = 4;
+
+                trades.Add(PoliceOfficer);
+            }
+
+            if(selection == 2)
+            {
+                Trade Gymnast = new Trade();
+
+                Gymnast.name = "Gymnast";
+
+                Gymnast.Agility = 5;
+
+                Gymnast.Charisma = 2;
+
+                Gymnast.Intelligence = 3;
+
+                Gymnast.Perception = 3;
+
+                Gymnast.Strength = 2;
+
+                Gymnast.Weaponry = 3;
+
+                trades.Add(Gymnast);
+            }
+
+            if(selection == 3)
+            {
+                Trade Actor = new Trade();
+
+                Actor.name = "Actor";
+
+                Actor.Agility = 3;
+
+                Actor.Charisma = 5;
+
+                Actor.Intelligence = 3;
+
+                Actor.Perception = 3;
+
+                Actor.Strength = 3;
+
+                Actor.Weaponry = 1;
+
+                trades.Add(Actor);
+            }
+
+            if (selection == 4)
+            {
+                Trade Unemployed = new Trade();
+
+                Unemployed.name = "Unemployed";
+
+                Unemployed.Agility = 3;
+
+                Unemployed.Charisma = 3;
+
+                Unemployed.Intelligence = 3;
+
+                Unemployed.Perception = 3;
+
+                Unemployed.Strength = 3;
+
+                Unemployed.Weaponry = 3;
+
+                trades.Add(Unemployed);
+            }
+
+            return trades[0];
+        }
+    }
+}
---
+++ Main Program/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Wasteland_Adventures.Data;
+
+namespace Wasteland_Adventures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The story
+
+            Console.WriteLine("The story begins with. . . Oh yes, what is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Awesome! " + name + " it is then");
+            Console.WriteLine("This story takes place in 2077. A few years after nuclear annihilation\n" +
+                "comes to the human race. Now we have to get you prepared for your survival \n" +
+                "in this new wasteland until your doom is inevitably upon you. \n" +
+                "By the way what was your profession before the wasteland? \n");
+
+            Console.WriteLine("Each profession has a set of stats that will help you in situations that play to your part! \n" +
+                "This will be important to remember! You can gain points to assign to your stats but only over time. \n " +
+                "Use your strengths or it's over! XP");
+
+            Console.WriteLine("Please pick from the list: \n" + " 0.Fire Fighter \n 1.Police Officer \n 2.Gymnast \n 3.Actor \n 4.Unemployed");
+
+            int selection;
+
+            int.TryParse(Console.ReadLine(), out selection);
+
+            Trade selectedJob = Trade.PlayerSelection(selection);
+
+            Console.WriteLine($"You chose {selectedJob.name}");
+
+            Console.WriteLine("Cool, let's hope your profession does you well. Who knows what's out there. \n " +
+                "Alright, back to reality! The story begins with you on top of your makeshift fallout car shelter \n " +
+                "fending off mutants hounds. The green glow of radiation from these 3 foot tall monsters is sickly looking. \n" +
+                "You're not sure how much radiation they may be emanating but it doesn't look good, nor does it feel good! You've \n" + 
+                "got to get out of here. You look for your rifle. It's on the ground near two of the hounds. \n" +
+                "");
+
+            //Loop for listing jobs
+        }
+    }
+}
